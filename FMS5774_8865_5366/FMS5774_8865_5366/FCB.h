a5c9fcae51a866f0e844634b53bf8b2f84a71236
Yitzhak <yurisho1@hotmail.com>
2014-05-02 13:03:36 +0300

    IO enum changes.

--- FMS5774_8865_5366/FMS5774_8865_5366/FCB.h
+++ FMS5774_8865_5366/FMS5774_8865_5366/FCB.h
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-enum IO
+enum IOState
 {
-	I,
-	O,
-	IandO
+	I, // Input
+	O, // Output
+	IO, // Input and Output
+	E // Edition.
 };
 
 class FCB
@@ -21,19 +22,19 @@ public:
 	unsigned int currRecNr;
 	unsigned int currSecNr;
 	unsigned int currRecNrInBuff;
-	IO io; //האם הפתיחה היא לכתיבה וקריאה
+	IOState iostate; //האם הפתיחה היא לכתיבה וקריאה
 	bool change;
 	FCB();
 	FCB(Disk *);
 
 	~FCB(void);
 	void closefile();
-	 void flushfile();
-	 void read(char *, [unsigned int]);
-	 void write(char *);
-	 void seek(unsigned int, int);
-	 void write(char *);
-	 void Delete();
-	 void update(char *);
+	void flushfile();
+	void read(char *, [unsigned int]);
+	void write(char *);
+	void seek(unsigned int, int);
+	void write(char *);
+	void Delete();
+	void update(char *);
 };
 
