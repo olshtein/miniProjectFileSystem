de5dd3e822aa3fd124b94d99c7193259f5855892
olshtein <olshtein@gmail.com>
2014-06-05 22:17:55 +0300

    add class's to record

---
+++ FMS5774_8865_5366/FMS5774_Cpp_CSharp_Adapter/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS5774_Cpp_CSharp_Adapter
+{
+    [StructLayout(LayoutKind.Sequential), Serializable]
+    public class Product
+    {
+        public int barcode;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string manufacturer;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string nameProduct;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string supplier;
+        public float price;
+
+        public Product(int Barcode,string Manufacturer,string name ,string Supplier, float Price)
+        {
+            barcode = Barcode;
+            manufacturer = Manufacturer;
+            nameProduct = name;
+            supplier = Supplier;
+            price = Price;
+        }
+
+        public static uint size()
+        {
+            return 12 * 3 + 4 + 4; //3 * string + int + float
+        }
+    }
+}
---
+++ FMS5774_8865_5366/FMS5774_Cpp_CSharp_Adapter/store.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS5774_Cpp_CSharp_Adapter
+{
+    [StructLayout(LayoutKind.Sequential), Serializable]
+    public class store
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string phone;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string name;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public String address;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public String city;
+
+        public store(string Phone, string Name, string Address, string City)
+        {
+            phone = Phone;
+            name = Name;
+            address = Address;
+            city = City;
+        }
+        public static uint size()
+        {
+            return 12 * 4;
+        }
+    }
+}
